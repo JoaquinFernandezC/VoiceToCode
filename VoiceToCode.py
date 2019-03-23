478ba8069ccb4754fea22e245a1dcc289527662c
nhurtado <nhurtado@miuandes.cl>
2019-03-23 19:47:10 -0300

    menu refactored.

--- VoiceToCode.py
+++ VoiceToCode.py
@@ -2,8 +2,32 @@ from pynput.keyboard import Key, Controller
 
 import speech_recognition as sr
 
-import os
+import os,time
 clear = lambda: os.system('cls')
+
+recognizer = sr.Recognizer()
+microphone = sr.Microphone()
+
+recognizer.energy_threshold = 50
+recognizer.dynamic_energy_threshold = False
+
+def voice_to_text(needed_text):
+    text = ""
+    print(needed_text)
+    while True:
+        with microphone as source:
+            print("...")
+            audio = recognizer.listen(source,phrase_time_limit=3)
+        try:
+            clear()
+            print(needed_text)
+            text = recognizer.recognize_google(audio)
+            if text != "":
+                break
+        except:
+            pass
+    return text
+    
 
 def print_menu(current):
     if len(current) > 0:
@@ -18,34 +42,43 @@ def print_menu(current):
         for key, value in menu.items() :
             print (key)
 
+def make_for():
+    keyboard.type("for i in :")
+    keyboard.press(Key.left)
+    
+def make_range():
+    keyboard.type("range()")
+    keyboard.press(Key.left)
 
-recognizer = sr.Recognizer()
-microphone = sr.Microphone()
+def make_var():
+    clear()
+    var_name = voice_to_text("Enter var name")
+    keyboard.type(var_name.lower().replace(" ", "_") + " =")
 
-recognizer.energy_threshold = 50
-recognizer.dynamic_energy_threshold = False
 
 keyboard = Controller()
 
-menu = { 'make' : {'for':1,'range':2,'variable': {'name variable':1}},
-        'jump' : {'start':1,'up':2,'down':3,'end':4} }
-
-current_menu = ['start']
-new_menu = []
+menu = { 'make' : {'for': make_for,
+                   'range': make_range,
+                   'bar': make_var},
+    
+        'jump' : {'start':1,
+                  'up':2,
+                  'down':3,
+                  'end':4} }
+current_menu = []
+clear()
+print_menu(current_menu)
 
 while True:
-    if current_menu != new_menu:
-        current_menu = new_menu[:]
-    
     text = ""
+    menu_lenght = len(current_menu)
+    clear()
+    print_menu(current_menu)
     with microphone as source:
-        #recognizer.adjust_for_ambient_noise(source)
-        clear()
-        print_menu(current_menu)
         print("...")
         audio = recognizer.listen(source,phrase_time_limit=3)
         
-
     try:
         clear()
         print_menu(current_menu)
@@ -59,28 +92,22 @@ while True:
                 
                 #debug
                 
-            elif current_menu == []:
-                if text == "make":
-                    new_menu.append("make")
-                elif text == "jump":
-                    new_menu.append("jump")
-                    
-            elif current_menu == ["make"]:
-                if text == "for" or text == "4" or text == "four":
-                    keyboard.type("for i in :")
-                    keyboard.press(Key.left)
-                    new_menu = []
-                elif text == "range":
-                    keyboard.type("range()")
-                    keyboard.press(Key.left)
-                    new_menu = []
-                elif text == "bar":
-                    new_menu = ['make','variable']
-                    
-            elif current_menu == ['make','variable']:
-                keyboard.type(text.lower().replace(" ", "_") + " =")
-                new_menu = []
-            print(text)
+            elif menu_lenght == 0:
+                if text in menu:
+                    current_menu.append(text)
+            elif menu_lenght > 0:
+                menu_option = menu[current_menu[0]]
+                for i in current_menu[1:]:
+                    menu_option = menu_option[i]
+                if text in menu_option:
+                    new_option = menu_option[text]
+                    if type(new_option) is dict:
+                        current_menu.append(text)
+                    else:
+                        menu_option[text]()
+                        current_menu = []
+                        
+            #print(text)
             text = ""
     except:
         pass
